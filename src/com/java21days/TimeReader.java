6a1ced533a52e23189b022a1bea9c47c4685102a
Alejandro Salas <asalas.wd@outlook.com>
2022-12-03 20:28:52 -0500

    Update frame size

--- src/com/java21days/TimeReader.java
+++ src/com/java21days/TimeReader.java
@@ -6,14 +6,17 @@ import java.net.URL;
 
 public class TimeReader extends JFrame {
     URL time;
-    Dimension dim = new Dimension(300, 300);
+    Dimension dim = new Dimension(500, 125);
 
     TimeReader(){
         super("Time Reader");
         setSize(dim);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        JTextArea textArea = new JTextArea(10, 5);
+        textArea.setText("This is my text area here");
+        textArea.setEditable(false);
+        add(textArea);
         setVisible(true);
     }
 
