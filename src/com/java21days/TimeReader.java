a82ef4b36b9d866cd3dc017cea4ea7678c26008a
Alejandro Salas <asalas.wd@outlook.com>
2022-12-03 20:25:09 -0500

    Create basic constructor

--- src/com/java21days/TimeReader.java
+++ src/com/java21days/TimeReader.java
@@ -1,4 +1,20 @@
 package com.java21days;
 
-public class TimeReader {
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public class TimeReader extends JFrame {
+    URL time;
+    Dimension dim = new Dimension(300, 300);
+
+    TimeReader(){
+        super("Time Reader");
+        setSize(dim);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+
 }
